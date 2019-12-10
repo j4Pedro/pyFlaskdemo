09f43b04261a4177896f25c0d48015a8de75ddad
Ora <adslaries@yahoo.com.tw>
2019-12-11 00:26:50 +0800

    新增Demo1

--- mywebsite.py
+++ mywebsite.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
 myflask = Flask(__name__)
 
 @myflask.route("/")
 def home():
-    return "Flask app test!!"
+    return render_template("welcome.html")
+
 
 if __name__ == "__main__":
     myflask.run()
